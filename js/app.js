4617dfc8964d0e2820d1c4ac1f3fea8de9b4e489
leopoldkwok <leopoldkwok@gmail.com>
2015-10-27 11:38:49 +0000

    Abstracting Grocery Items

--- js/app.js
+++ js/app.js
@@ -19,22 +19,33 @@ app.config(function($routeProvider) {
         })
 });
 
-app.controller("HomeController", ["$scope", function($scope) {
-	$scope.appTitle="Grocery List";
-}]);
+app.service("GroceryService", function() {
+    var groceryService ={};
 
-app.controller("GroceryListItemsController", ["$scope", "$routeParams", function($scope, $routeParams) {
-
-	$scope.groceryItems = [
-		{completed: true, itemName: 'milk', date: '2014-10-01'},
-		{completed: true, itemName: 'cookies', date: '2014-10-01'},
+    groceryService.groceryItems = [
+        {completed: true, itemName: 'milk', date: '2014-10-01'},
+        {completed: true, itemName: 'cookies', date: '2014-10-01'},
         {completed: true, itemName: 'ice cream', date: '2014-10-02'},
         {completed: true, itemName: 'potatoes', date: '2014-10-02'},
         {completed: true, itemName: 'cereal', date: '2014-10-03'},
         {completed: true, itemName: 'bread', date: '2014-10-03'},
         {completed: true, itemName: 'eggs', date: '2014-10-04'},
         {completed: true, itemName: 'tortillas', date: '2014-10-04'}
-    ]
+    ];
+
+    return groceryService;
+
+});
+
+app.controller("HomeController", ["$scope", "GroceryService", function($scope, GroceryService) {
+	$scope.appTitle= GroceryService.groceryItems[0].itemName;
+    
+    
+}]);
+
+app.controller("GroceryListItemsController", ["$scope", "$routeParams", "GroceryService", function($scope, $routeParams, GroceryService) {
+
+	$scope.groceryItems = GroceryService.groceryItems;
 
     $scope.rp = "Route Parameter Value: " + $routeParams.id + $routeParams.cat;
 
